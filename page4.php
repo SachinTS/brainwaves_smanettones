15d2c5763a874e6f5c2ceed3c1cd9bbb2cb2f614
Sachin TS <sachintshebbar@gmail.com>
2014-11-09 00:19:49 +0530

    nodu

--- page4.php
+++ page4.php
@@ -1,4 +1,19 @@
-<!DOCTYPE html>
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "digichq";
+//$userId = $_POST['username'];
+//$passd = $_POST['password'];
+// Create connection
+$conn = mysqli_connect($servername, $username, $password, $dbname);
+// Check connection
+session_start();
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
+}
+?>
+
 <html lang='en'>
 <head>
     <meta charset="UTF-8" /> 
@@ -25,7 +40,28 @@
 <form name="signup" onsubmit="return validateform()" action="">
   <h1>DETAILS OF YOUR BANK ACCOUNTS</h1>
   <div class="inset">
-    <input type="submit" name="submit" id="submit" value="Login">
+    
+<?php
+//session_start();
+$sql = "SELECT * FROM userbank where email = 'nagaravali.t@gmail.com'";
+$result = mysqli_query($conn, $sql);
+if (mysqli_num_rows($result) > 0) {
+    // output data of each row
+    while($row = mysqli_fetch_assoc($result)) {
+        $link =  $row["bname"];//. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+        $link_address = '#';
+        echo '<a href="localhost/$row["bname"]">Link</a>';
+    }
+} else {
+    echo "0 results";
+}
+
+mysqli_close($conn);
+
+?>
+
+  </div>
+    <input type="submit" name="submit" id="submit" value="OK">
   </p>
 </form>
 
